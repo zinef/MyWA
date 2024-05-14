8c4630c1696dcfcbb59b45d95f149f28107b51ee
FODIL zine-eddine <zineeddine.fodil99@gmail.com>
2024-05-14 23:58:54 +0200

    Exploring the starter kit ...

---
+++ WA_img_test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="WA_img_test" tilewidth="32" tileheight="32" tilecount="1102" columns="29">
+ <image source="../../../Desktop/CV's/00000PORTRAIT_00000_BURST20220911185633235.jpg" width="932" height="1243"/>
+</tileset>
